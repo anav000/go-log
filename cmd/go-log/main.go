95958e0dca815430de7988b8a7f986c0dc1732ae
Axel Navarro <axel.navarro@epitech.eu>
2024-06-28 20:18:44 +0200

    update main.go
    mv execCommand() to tools/execTools.go + add docstrings

--- cmd/go-log/main.go
+++ cmd/go-log/main.go
@@ -2,67 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os/exec"
-	"strconv"
-	"strings"
 	"time"
 
 	"go-log/cli"
 	"go-log/cmd"
 	"go-log/tools"
-
-	"github.com/rs/zerolog"
 )
 
-func execCommand(ctx context.Context, logger zerolog.Logger, errChan chan error, done chan struct{}) int {
-	execCmd := exec.CommandContext(ctx, "sh", "-c", cmd.Command)
-	logger.Debug().Msgf("starting running the command: %s", cmd.Command)
-	stdoutPipe, errStdout := tools.CreatePipe(execCmd, "stdout")
-	stderrPipe, errStderr := tools.CreatePipe(execCmd, "stderr")
-	stdoutChan := make(chan string)
-	stderrChan := make(chan string)
-
-	defer stdoutPipe.Close()
-	defer stderrPipe.Close()
-
-	if errStdout != nil {
-		errChan <- fmt.Errorf("error creating stdout pipe: %v", errStdout)
-		return 1
-	}
-	if errStderr != nil {
-		errChan <- fmt.Errorf("error creating stdout pipe: %v", errStderr)
-		return 1
-	}
-
-	timeDuration := time.Now()
-	if err := execCmd.Start(); err != nil {
-		errChan <- fmt.Errorf("error starting command: %v", err)
-		return 1
-	}
-
-	go tools.ReadStandardBuffer(stdoutChan, stdoutPipe)
-	go tools.ReadStandardBuffer(stderrChan, stderrPipe)
-
-	if err := execCmd.Wait(); err != nil {
-		errChan <- fmt.Errorf("error waiting for command: %v", err)
-		return execCmd.ProcessState.ExitCode()
-	}
-
-	stdoutStr := <-stdoutChan
-	stderrStr := <-stderrChan
-	exitCode := execCmd.ProcessState.ExitCode()
-	logger.Debug().Msgf("the command has been executed in %s", time.Since(timeDuration))
-	if stdoutStr != "" {
-		logger.Info().Str("exitCode", strconv.Itoa(exitCode)).Msg(strings.TrimSuffix(stdoutStr, "\n"))
-	}
-	if stderrStr != "" {
-		logger.Info().Str("exitCode", strconv.Itoa(exitCode)).Msg(strings.TrimSuffix(stderrStr, "\n"))
-	}
-	close(done)
-	return exitCode
-}
-
+// main is the entry point of the program.
 func main() {
 	running := true
 	logger := cli.ParseFlag()
@@ -71,19 +18,20 @@ func main() {
 	timeout := time.Duration(cmd.TimeOut) * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
+	defer close(errChan)
 
 	logger.Trace().Msg("the program is starting")
-	go execCommand(ctx, logger, errChan, done)
+	go tools.ExecCommand(ctx, logger, errChan, done)
 
 	for running {
 		select {
 		case <-done:
-			logger.Trace().Msg("the go routine has finished")
+			logger.Trace().Msg("the command has finished")
 			running = false
 		case err := <-errChan:
-			logger.Fatal().Err(err).Msg("an error has occured during the execution of the command")
+			logger.Fatal().Err(err).Msg("an error has occurred")
 		case <-ctx.Done():
-			logger.Fatal().Err(ctx.Err()).Msg("the routine badly ended")
+			logger.Fatal().Err(ctx.Err()).Msg("the command has timed out")
 		}
 	}
 	logger.Trace().Msg("the program is ending")
--- tools/execTools.go
+++ tools/execTools.go
@@ -2,12 +2,22 @@ package tools
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"go-log/cmd"
 	"io"
 	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
 )
 
-func CreatePipe(cmd *exec.Cmd, pipeType string) (io.ReadCloser, error) {
+// CreatePipe creates a pipe for the specified command.
+// The pipeType parameter specifies the type of pipe to create (stdout or stderr).
+// The function returns the pipe and an error if one occurred.
+func createPipe(cmd *exec.Cmd, pipeType string) (io.ReadCloser, error) {
 	var pipe io.ReadCloser
 	var err error
 
@@ -26,10 +36,65 @@ func CreatePipe(cmd *exec.Cmd, pipeType string) (io.ReadCloser, error) {
 	return pipe, nil
 }
 
-func ReadStandardBuffer(stdChan chan string, stdPipe io.ReadCloser) {
+// ReadStandardBuffer reads the standard buffer and sends it to the specified channel.
+// The function closes the channel when the buffer is read.
+func readStandardBuffer(stdChan chan string, stdPipe io.ReadCloser) {
 	var stdBuffer bytes.Buffer
 
 	io.Copy(&stdBuffer, stdPipe)
 	stdChan <- stdBuffer.String()
 	close(stdChan)
+}
+
+// execCommand executes the command specified in the command line.
+// The function returns the exit code of the command.
+// If an error occurs, the function sends the error to the specified channel.
+// The function also sends a signal to the done channel when the command has finished.
+func ExecCommand(ctx context.Context, logger zerolog.Logger, errChan chan error, done chan struct{}) int {
+	execCmd := exec.CommandContext(ctx, "sh", "-c", cmd.Command)
+	execCmd.Env = append(execCmd.Env, cmd.Env...)
+	stdoutPipe, errStdout := createPipe(execCmd, "stdout")
+	stderrPipe, errStderr := createPipe(execCmd, "stderr")
+	stdoutChan := make(chan string)
+	stderrChan := make(chan string)
+
+	defer stdoutPipe.Close()
+	defer stderrPipe.Close()
+	defer close(done)
+
+	logger.Debug().Msgf("starting running the command: %s", cmd.Command)
+	if errStdout != nil {
+		errChan <- fmt.Errorf("error creating stdout pipe: %v", errStdout)
+		return 1
+	}
+	if errStderr != nil {
+		errChan <- fmt.Errorf("error creating stdout pipe: %v", errStderr)
+		return 1
+	}
+
+	timeDuration := time.Now()
+	if err := execCmd.Start(); err != nil {
+		errChan <- fmt.Errorf("error starting command: %v", err)
+		return 1
+	}
+
+	go readStandardBuffer(stdoutChan, stdoutPipe)
+	go readStandardBuffer(stderrChan, stderrPipe)
+
+	if err := execCmd.Wait(); err != nil {
+		errChan <- fmt.Errorf("error waiting for command: %v", err)
+		return execCmd.ProcessState.ExitCode()
+	}
+
+	stdoutStr := <-stdoutChan
+	stderrStr := <-stderrChan
+	exitCode := execCmd.ProcessState.ExitCode()
+	logger.Debug().Msgf("command finished in %s", time.Since(timeDuration))
+	if stdoutStr != "" {
+		logger.Info().Str("exitCode", strconv.Itoa(exitCode)).Msg(strings.TrimSuffix(stdoutStr, "\n"))
+	}
+	if stderrStr != "" {
+		logger.Info().Str("exitCode", strconv.Itoa(exitCode)).Msg(strings.TrimSuffix(stderrStr, "\n"))
+	}
+	return exitCode
 }
